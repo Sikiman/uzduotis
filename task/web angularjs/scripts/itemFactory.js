e845f4905bff53221c590d62dda7eb33a0ab9bb8
Sikiman <93723175+Sikiman@users.noreply.github.com>
2021-11-08 21:56:28 +0200

    Add files via upload

---
+++ task/web angularjs/scripts/itemFactory.js
@@ -0,0 +1,46 @@
+angular
+    .module('itemApp')
+    .factory('itemFactory', function ($http) {
+
+        function getItems() {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8080/items'
+            });
+        }
+
+        function postItem(newItem) {
+            $http({
+                url: 'http://localhost:8080/additem',
+                method: "POST",
+                data: newItem
+            }).then(function (response) {
+                console.log(response);
+            },
+                function (response) {
+                    console.log(response);
+                });
+        }
+
+        function deleteItem(itemToDelete) {
+            $http({
+                url: 'http://localhost:8080/deleteitem',
+                method: "DELETE",
+                headers: {
+                    'Content-type': 'application/json;charset=utf-8'
+                },
+                data: itemToDelete
+            }).then(function (response) {
+                console.log(response);
+            },
+                function (response) {
+                    console.log(response);
+                });
+        }
+
+        return {
+            getItems: getItems,
+            postItem: postItem,
+            deleteItem: deleteItem
+        }
+    });
